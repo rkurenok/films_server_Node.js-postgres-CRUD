626cd84064690c09a1eb2687716066c5a940e053
rkurenok <48415490+rkurenok@users.noreply.github.com>
2023-03-13 22:47:55 +0300

    fixed a bug for "put" films

--- controller/film.controller.js
+++ controller/film.controller.js
@@ -27,7 +27,7 @@ class FilmController {
         }
 
         const filmId = newFilm.rows[0].film_id;
-        
+
         for await (const el of genre) {
             await db.query('INSERT INTO film_genre values ($1, $2) RETURNING *', [filmId, el]);
         }
@@ -69,19 +69,6 @@ class FilmController {
             return;
         }
 
-        const genreArr = [];
-        // проверяем, существуют ли жанры по новый id
-        for (const el of genre) {
-            genreArr.push(await db.query('SELECT * FROM genre where genre_id = $1', [el]));
-        }
-
-        for (let i = 0; i < genreArr.length; i++) {
-            if (genreArr[i].rows.length === 0) {
-                res.send("Такого жанра не существует");
-                return;
-            }
-        }
-
         const film = await db.query(`UPDATE film set title = $1, creation_year = $2 where film_id = $3 RETURNING *`, [name, year, id]); // изменяем параметры таблицы film
 
         if (film.rows.length == 0) {
@@ -89,14 +76,33 @@ class FilmController {
             return;
         }
 
-        await db.query(`DELETE FROM film_genre where film_id = $1`, [id]); // удаляем предыдущие связи film_genre
+        if (genre != undefined && genre.length !== 0) {
+            const genreArr = [];
+            // проверяем, существуют ли жанры по новый id
+            for (const el of genre) {
+                genreArr.push(await db.query('SELECT * FROM genre where genre_id = $1', [el]));
+            }
 
-        for (const el of genre) {
-            await db.query('INSERT INTO film_genre values ($1, $2) RETURNING *', [id, el]); // добавляем новые связи film_genre
+            for (let i = 0; i < genreArr.length; i++) {
+                if (genreArr[i].rows.length === 0) {
+                    res.send("Такого жанра не существует");
+                    return;
+                }
+            }
+
+            await db.query(`DELETE FROM film_genre where film_id = $1`, [id]); // удаляем предыдущие связи film_genre
+            for (const el of genre) {
+                await db.query('INSERT INTO film_genre values ($1, $2) RETURNING *', [id, el]); // добавляем новые связи film_genre
+            }
+
+            const changedFilm = await db.query(_basicQuery + `where film.film_id = $1 group by film.film_id`, [id]);
+            res.send(changedFilm.rows[0]);
         }
-
-        const changedFilm = await db.query(_basicQuery + `where film.film_id = $1 group by film.film_id`, [id]);
-        res.send(changedFilm.rows[0]);
+        else {
+            await db.query(`DELETE FROM film_genre where film_id = $1`, [id]); // удаляем предыдущие связи film_genre
+            film.rows[0].genre_titles = null; // если жанры не были заданы - ставим null
+            res.send(film.rows[0]);
+        }
     }
 
     async deleteFilm(req, res) {
@@ -115,6 +121,6 @@ class FilmController {
 }
 
 const _basicQuery = `SELECT film.film_id, film.title, film.creation_year, string_agg(genre.title, ', ') as genre_titles ` +
-`FROM film LEFT JOIN film_genre ON film_genre.film_id=film.film_id LEFT JOIN genre ON film_genre.genre_id=genre.genre_id `;
+    `FROM film LEFT JOIN film_genre ON film_genre.film_id=film.film_id LEFT JOIN genre ON film_genre.genre_id=genre.genre_id `;
 
 module.exports = new FilmController();
